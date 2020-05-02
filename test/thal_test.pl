b88b7d3e577af488719df343d4bba048a5907e73
Andreas Untergasser <andreas@untergasser.de>
2020-05-02 09:10:23 +0200

    tt

--- test/thal_test.pl
+++ test/thal_test.pl
@@ -138,7 +138,7 @@ sub main() {
         . " --show-reachable=yes --log-file=ntthal.%0.4d.valg ";
 
     if ($winFlag) {
-        $exe = '../src/ntthal.exe';
+        $exe = '..\\src\\ntthal.exe';
     }
 
     # Look for the ntthal executable
@@ -151,7 +151,7 @@ sub main() {
     # Test error handling on over-long input sequence:
     print "Error handling of too-long sequence...\n";
     my @foo=();
-    my $cmd = "$valgrind_prefix $exe -s1 ACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAGTG -s2 ACGTGTTCGTCGTATGACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAG -a ANY > thal.tmp 2>&1";
+    my $cmd = "$valgrind_prefix " . $exe  . "-s1 ACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAGTG -s2 ACGTGTTCGTCGTATGACGTGTTCGTCGTAGACGTGTTCGTCGTAGACGTGTTCGTCGTAG -a ANY > thal.tmp 2>&1";
     my $r = _nowarn_system($cmd);
     open X, 'thal.tmp';         # Get the test output
     @foo = <X>;                 # Snarf it
