b5b347c76f40c53ad779bce0cb81bf606f0cf312
Andreas Untergasser <andreas@untergasser.de>
2020-05-02 17:53:08 +0200

    hhh

--- src/paranoid_tests_a.sh
+++ src/paranoid_tests_a.sh
@@ -7,8 +7,4 @@ echo; echo; echo 'STARTING 2ND SET OF TESTS (-O3)'; echo; echo;
 make O_OPTS=-O3
 make test
 make clean
-echo; echo; echo 'STARTING 3RD SET OF TESTS (-O0 and valgrind)'; echo; echo;
-make O_OPTS=-O0
-cd ../test
-make TESTOPTS=--valgrind
 
